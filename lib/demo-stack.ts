139630667e8653eba9d2f236ec0688e78595ae28
Nino <nino@DESKTOP-44HFBIK>
2023-03-11 11:32:20 +0100

    added lambda

--- lib/demo-stack.ts
+++ lib/demo-stack.ts
@@ -1,16 +1,22 @@
-import * as cdk from 'aws-cdk-lib';
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
+import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class DemoStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class DemoStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const hello = new Function(this, 'HelloHandler', {
+      runtime: Runtime.NODEJS_14_X,
+      code: Code.fromAsset('lambda'),
+      handler: 'hello.handler'
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'DemoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // defines an API Gateway REST API resource backed by our "hello" function.
+    new LambdaRestApi(this, 'ApiGateway', {
+      handler: hello
+    });
   }
 }
